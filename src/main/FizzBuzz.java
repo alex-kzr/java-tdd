30d37e64ca5e7c66748de342b8bddc11d9e65bb4
DESKTOP-OMFP4VN\admin <aleksey.kozyura@gmail.com>
2022-01-06 15:38:44 +1000

    Change check method to return Buzz when diversable by 5 value is passed

--- src/main/FizzBuzz.java
+++ src/main/FizzBuzz.java
@@ -4,6 +4,8 @@ public class FizzBuzz {
     public String check(int value) {
         if(value % 3 == 0) {
             return "Fizz";
+        } else if (value % 5 == 0) {
+            return "Buzz";
         }
         return String.valueOf(value);
     }
