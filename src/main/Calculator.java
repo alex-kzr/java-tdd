6cd9d34a9be2a567e418d5d634aa871206a19b34
DESKTOP-OMFP4VN\admin <aleksey.kozyura@gmail.com>
2022-01-06 16:48:05 +1000

    Change addStrings method to pass test

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -1,12 +1,14 @@
 package main;
 
+import java.math.BigDecimal;
+
 public class Calculator {
     public int add(int first, int second) {
         return first + second;
     }
 
     public String addStrings(String first, String second) {
-        return null;
+        return new BigDecimal(first).add(new BigDecimal(second)).toString();
     }
 
     public int subtract(int first, int second) {
