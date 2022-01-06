a6259e88292ed6c155835a61aeaa0779937a7bef
DESKTOP-OMFP4VN\admin <aleksey.kozyura@gmail.com>
2022-01-06 17:30:08 +1000

    Change subtract method to pass test

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -14,11 +14,13 @@ public class Calculator {
         return firstValue.add(secondValue).toString();
     }
 
-    public int subtract(int first, int second) {
-        return first - second;
-    }
-
     public String subtract(String first, String second) {
-        return new BigDecimal(first).subtract(new BigDecimal(second)).toString();
+        BigDecimal firstValue = new BigDecimal(first);
+        BigDecimal secondValue = new BigDecimal(second);
+
+        if(firstValue.scale() > 2 || secondValue.scale() >2 ){
+            throw new IllegalArgumentException("Too many decimal places.");
+        }
+        return firstValue.subtract(secondValue).toString();
     }
 }
