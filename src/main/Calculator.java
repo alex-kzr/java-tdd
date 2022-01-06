79867f64f678ddc9499c8ddbcaf3bbf1e6748d27
DESKTOP-OMFP4VN\admin <aleksey.kozyura@gmail.com>
2022-01-06 17:07:09 +1000

    Change add method to pass the test

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -5,7 +5,13 @@ import java.math.BigDecimal;
 public class Calculator {
 
     public String add(String first, String second) {
-        return new BigDecimal(first).add(new BigDecimal(second)).toString();
+        BigDecimal firstValue = new BigDecimal(first);
+        BigDecimal secondValue = new BigDecimal(second);
+
+        if(firstValue.scale() > 2 || secondValue.scale() >2 ){
+            throw new IllegalArgumentException("Too many decimal places.");
+        }
+        return firstValue.add(secondValue).toString();
     }
 
     public int subtract(int first, int second) {
