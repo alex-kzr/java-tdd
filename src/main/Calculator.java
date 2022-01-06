0fa000f1c83e3f67e3e536c5c42265cdccd0a789
DESKTOP-OMFP4VN\admin <aleksey.kozyura@gmail.com>
2022-01-06 16:51:23 +1000

    Refactor second test and delete add method

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -3,9 +3,6 @@ package main;
 import java.math.BigDecimal;
 
 public class Calculator {
-    public int add(int first, int second) {
-        return first + second;
-    }
 
     public String addStrings(String first, String second) {
         return new BigDecimal(first).add(new BigDecimal(second)).toString();
