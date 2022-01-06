c74379ab7b469fe254e3e28805cb3876363005ae
DESKTOP-OMFP4VN\admin <aleksey.kozyura@gmail.com>
2022-01-06 15:54:23 +1000

    Change check method to pass test

--- src/main/FizzBuzz.java
+++ src/main/FizzBuzz.java
@@ -3,6 +3,9 @@ package main;
 public class FizzBuzz {
     public String check(int value) {
         if(value % 3 == 0) {
+            if(value % 5 == 0){
+                return "FizzBuzz";
+            }
             return "Fizz";
         } else if (value % 5 == 0) {
             return "Buzz";
