0a73ddf8526922bc799ad00a394d7edbc6a7aefd
Alex.Kozyura <aleksey.kozyura@gmail.com>
2022-01-11 13:56:44 +1000

    Take out validation creation to separate setUp method

--- src/test/ValidationTest.java
+++ src/test/ValidationTest.java
@@ -2,13 +2,19 @@ package test;
 
 import main.Validation;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ValidationTest {
+    Validation validation;
+
+    @Before
+    public void setUp() {
+        validation = new Validation();
+    }
 
     @Test
     public void shouldReturnAnInstanceOfAValidation(){
-        Validation validation = new Validation();
         Assert.assertNotNull(validation);
     }
 }
