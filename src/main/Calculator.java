f8e214592590e8d891276ea072c009298f0e9d71
Alex.Kozyura <aleksey.kozyura@gmail.com>
2022-01-12 23:45:29 +1000

    Add fail test for entering and dropping

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -12,4 +12,10 @@ public class Calculator {
     public void setAccumulator(BigDecimal value) {
         accumulator = value;
     }
+
+    public void enter() {
+    }
+
+    public void drop() {
+    }
 }
