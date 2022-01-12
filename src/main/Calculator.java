8ba2fce8eaddaeba5045336abeff8685add8a327
Alex.Kozyura <aleksey.kozyura@gmail.com>
2022-01-12 23:53:35 +1000

    Refactor accumulator and tests to Stack. Change enter and drop to pass test.

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -1,21 +1,31 @@
 package main;
 
 import java.math.BigDecimal;
+import java.util.Stack;
 
 public class Calculator {
-    private BigDecimal accumulator = BigDecimal.ZERO;
+
+    private Stack<BigDecimal> accumulator = new Stack<>();
 
     public BigDecimal getAccumulator() {
-        return accumulator;
+        if(accumulator.size() == 0) {
+            return BigDecimal.ZERO;
+        }
+        return accumulator.peek();
     }
 
     public void setAccumulator(BigDecimal value) {
-        accumulator = value;
+        if(accumulator.size() > 0) {
+            accumulator.pop();
+        }
+        accumulator.push(value);
     }
 
     public void enter() {
+        accumulator.push(getAccumulator());
     }
 
     public void drop() {
+        accumulator.pop();
     }
 }
