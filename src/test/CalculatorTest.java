cad7b6a5f6b8e24f2b050eba13e17d7a3161daf2
Alex.Kozyura <aleksey.kozyura@gmail.com>
2022-01-12 13:41:05 +1000

    Refactor CalculatorTest - move calculator creation under @Before

--- src/test/CalculatorTest.java
+++ src/test/CalculatorTest.java
@@ -2,21 +2,27 @@ package test;
 
 import main.Calculator;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.math.BigDecimal;
 
 public class CalculatorTest {
 
+    private Calculator calculator;
+
+    @Before
+    public void createCalculator(){
+        calculator = new Calculator();
+    }
+
     @Test
     public void shouldReturnAccumulatorOfZeroWhenCreated() {
-        Calculator calculator = new Calculator();
         Assert.assertEquals(BigDecimal.ZERO, calculator.getAccumulator());
     }
 
     @Test
     public void shouldReturnSetAccumulatorValue() {
-        Calculator calculator = new Calculator();
         BigDecimal value = new BigDecimal(23);
         calculator.setAccumulator(value);
         Assert.assertEquals(value, calculator.getAccumulator());
